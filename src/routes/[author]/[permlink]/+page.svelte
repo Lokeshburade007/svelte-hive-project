f395f6435e54d9e95d5ed3c7a6a2994411680e80
Lokesh Burade <lokeshburade007@gmail.com>
2025-11-06 13:20:01 +0530

    Post Body rendered using hive content Rendered package

--- src/routes/[author]/[permlink]/+page.svelte
+++ src/routes/[author]/[permlink]/+page.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
+	// @ts-ignore
+	import { DefaultRenderer } from '@hiveio/content-renderer';
 	let { data } = $props();
+
+	const renderer = new DefaultRenderer({
+		baseUrl: 'https://hive.blog/',
+		breaks: true,
+		skipSanitization: false,
+		allowInsecureScriptTags: false,
+		addNofollowToLinks: true,
+		doNotShowImages: false,
+		assetsWidth: 640,
+		assetsHeight: 480,
+		imageProxyFn: (url: string) => url,
+		usertagUrlFn: (account: string) => '/@' + account,
+		hashtagUrlFn: (hashtag: string) => '/trending/' + hashtag,
+		isLinkSafeFn: (url: string) => true,
+		addExternalCssClassToMatchingLinksFn: (url: string) => true,
+		ipfsPrefix: 'https://ipfs.io/ipfs/'
+	});
+
+	// svelte-ignore non_reactive_update
+		let renderedBody = '';
+	if (data?.post?.body) {
+		renderedBody = renderer.render(data.post.body);
+	}
 </script>
 
 <main class="p-4 space-y-4">
@@ -12,6 +37,6 @@
 	<h1 class="text-2xl font-bold text-gray-300">{data.post.title}</h1>
 
 	<div class="prose max-w-none text-gray-400">
-		{@html data.post.body}
+		{@html renderedBody}
 	</div>
 </main>
